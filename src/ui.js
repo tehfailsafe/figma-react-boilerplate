672288198ff499cd5c82014a60d7a1ad8b415c1c
Mike <mike@failsafedesign.com>
2024-11-18 12:52:38 -0600

    Add a live dev server

--- src/ui.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('react-page'));
-root.render(React.createElement(App, null));
--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,63 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 const path = require('path');
 
-module.exports = (env, argv) => ({
-  mode: argv.mode === 'production' ? 'production' : 'development',
-  devtool: argv.mode === 'production' ? false : 'inline-source-map',
-  entry: {
-    ui: './src/ui.tsx',
-    code: './code.ts',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+  const isDevServer = process.env.WEBPACK_SERVE;
+
+  const config = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'inline-source-map' : false,
+    entry: {
+      ui: './src/ui.tsx',
+      ...(isDevServer ? {} : { code: './code.ts' }),
     },
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/ui.html',
-      filename: 'ui.html',
-      chunks: ['ui'],
-      cache: false,
-      inject: 'body',
-    }),
-    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/]),
-  ],
-});
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.jsx', '.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/ui.html',
+        filename: isDevServer ? 'index.html' : 'ui.html',
+        chunks: ['ui'],
+        cache: false,
+        inject: 'body',
+      }),
+      ...(isDevServer ? [] : [new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/])]),
+    ],
+  };
+
+  if (isDevServer) {
+    config.devServer = {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      port: 3000,
+      hot: true,
+      compress: true,
+      open: true,
+    };
+  }
+
+  return config;
+};
