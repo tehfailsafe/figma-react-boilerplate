522fb8c85d62a0d581436283f31df784c58904d0
Mike <mike@failsafedesign.com>
2024-11-17 19:35:40 -0600

    working version

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Hello Figma!</h2>
+      <p>This is a basic React plugin.</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/ui.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('react-page'));
+root.render(React.createElement(App, null));
